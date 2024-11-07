028c85d7621bb4f4ea1532fdd689a8e5f506640c
Sadikur Rahman <srsadik365@gmail.com>
2024-11-07 00:31:17 -0800

    first commit.

--- lib/ui/utils/assets_path.dart
+++ lib/ui/utils/assets_path.dart
@@ -1,6 +1,5 @@
 class AssetsPath{
   static const String _imagePath = 'assets/images';
-
   static const String backgroundSvg = '$_imagePath/background.svg';
   static const String logoSvg = '$_imagePath/app_logo.svg';
 }

a602b08fc8a8ba7bf208bbbc0ff633ad7e7f67d7
Sadikur Rahman <srsadik365@gmail.com>
2024-11-15 22:45:46 -0800

    .o

--- lib/ui/controllers/sign_up_controller.dart
+++ lib/ui/controllers/sign_up_controller.dart
@@ -8,6 +8,7 @@ class SignUpController extends GetxController {
   String? _errorMassage;
   bool get inProgress => _inProgress;
   String? get errorMassage => _errorMassage;
+  
 
   Future<bool> signUp(
     String email,
